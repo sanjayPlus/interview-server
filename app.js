0e617be091c0754d6d07ad6f48d11d44355ad5b1
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-12-19 12:47:21 +0530

    test

--- app.js
+++ app.js
@@ -1,130 +1,149 @@
-//express server
+// express server
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const data = require('./data');
-//middleware
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("Connected to DB");
-})
+});
 
 const userSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
-
+    name: String,
+    email: String,
+    password: String
 });
-const User = mongoose.model("User",userSchema);
-const userAuth = (req,res,next)=>{
+
+const User = mongoose.model("User", userSchema);
+
+const userAuth = (req, res, next) => {
     const token = req.headers["x-access-token"];
-    if(!token){
+    if (!token) {
         return res.status(400).send("Token not found");
     }
     try {
-        const decoded = jwt.verify(token,"secret");
+        const decoded = jwt.verify(token, "secret");
         req.user = decoded;
         next();
     } catch (error) {
         return res.status(400).send("Invalid Token");
     }
-}
+};
 
-app.get("/",async(req,res)=>{
+app.get("/", async (req, res) => {
     res.send("Hello World");
-})
-app.post("/api/register",async(req,res)=>{
+});
+
+app.post("/api/register", async (req, res) => {
     try {
-        
-        const {name,email,password} = req.body;
-        //validate
-        if(!name || !email || !password){
+        const { name, email, password } = req.body;
+
+        // validate
+        if (!name || !email || !password) {
             return res.status(400).send("All fields are required");
         }
-        //check if user already exists
-        const user = User.findOne({email});
-        if(user){
+
+        // check if user already exists
+        const existingUser = await User.findOne({ email: email });
+        if (existingUser) {
             return res.status(400).send("User already exists");
         }
-        //create user
-        const newUser = new User({name,email,password});
-        await newUser.save();
-        const token = jwt.sign({id:newUser._id},"secret");
 
-        res.status(200).json({token});
+        // create user
+        const newUser = new User({ name, email, password });
+        await newUser.save();
+        const token = jwt.sign({ id: newUser._id }, "secret");
+
+        res.status(200).json({ token });
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).send(error.message);
     }
-})
-app.post("/api/login",async(req,res)=>{
+});
+
+app.post("/api/login", async (req, res) => {
     try {
-        const {email,password} = req.body;
-        //validate
-        if(!email || !password){
+        const { email, password } = req.body;
+
+        // validate
+        if (!email || !password) {
             return res.status(400).send("All fields are required");
         }
-        //check if user already exists
-        const user = await User.findOne({email});
-        if(!user){
-            return res.status(400).send("User does not exists");
+
+        // check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).send("User does not exist");
         }
-        //check password
-        if(user.password !== password){
+
+        // check password
+        if (user.password !== password) {
             return res.status(400).send("Invalid Credentials");
         }
-        //create token
-        const token = jwt.sign({id:user._id},"secret");
 
-        res.status(200).json({token});
+        // create token
+        const token = jwt.sign({ id: user._id }, "secret");
+
+        res.status(200).json({ token });
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).send(error.message);
     }
-})
-app.get("/api/users",userAuth,async(req,res)=>{
+});
+
+app.get("/api/users", userAuth, async (req, res) => {
     const userId = req.user.id;
-    const users = await User.findById(userId);
-    res.status(200).send(users);
-})
-app.put("/api/update-user",userAuth,async(req,res)=>{
+    const user = await User.findById(userId);
+    res.status(200).json(user);
+});
+
+app.put("/api/update-user", userAuth, async (req, res) => {
     try {
         const userId = req.user.id;
-        const {name,password} = req.body;
+        const { name, password } = req.body;
         const user = await User.findById(userId);
-        if(!user){
-            return res.status(400).send("User does not exists");
+
+        if (!user) {
+            return res.status(400).send("User does not exist");
         }
-        if(name){
+
+        if (name) {
             user.name = name;
         }
-        if(password){
+
+        if (password) {
             user.password = password;
         }
+
         await user.save();
         res.status(200).send("User updated");
     } catch (error) {
-        res.status(401).send(error);
+        res.status(401).send(error.message);
     }
-})
-app.delete('/api/delete-user',userAuth,async(req,res)=>{
+});
+
+app.delete('/api/delete-user', userAuth, async (req, res) => {
     try {
         const userId = req.user.id;
-        const user = await User.findByIdAndDelete(userId);
+        await User.findByIdAndDelete(userId);
         res.status(200).send("User deleted");
     } catch (error) {
-        res.status(401).send(error);
+        res.status(401).send(error.message);
     }
-})
-app.get("/api/items",async(req,res)=>{
-    res.status(200).json(data);
-})
-app.get("/api/protected",userAuth,async(req,res)=>{
-    res.status(200).json({message:"This is protected route"});
-})
+});
 
-app.listen(3000,()=>{
+app.get("/api/items", (req, res) => {
+    res.status(200).json(data);
+});
+
+app.get("/api/protected", userAuth, (req, res) => {
+    res.status(200).json({ message: "This is a protected route" });
+});
+
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
-})
+});
