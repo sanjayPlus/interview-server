e74bf21266845e19282d64efcdbdd8ec4147a1cc
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-02-12 09:36:16 +0530

    hi

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("Connected to DB");
 });
 
+
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
